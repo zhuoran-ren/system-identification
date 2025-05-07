cd808e35cad7e3a75c1296fea4af747aaa30145a
zhua zhua <zhuoran.ren@epfl.ch>
2025-05-07 22:03:53 +0800

    change the main.py

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ from src.system_identification import SysIdentification
 from src.utils import *
 from src.visualization import Visualization
 
-
 def main(file_name):
     # load the file, including input and output signals
     # and the parameters for the excitation signals
-    freq_range, f, N, p, m, u, y = load_identification_data(file_name)
+    freq_range, f, N, p, m, u_sysid, us, y_arm1, y_arm2, y_arm3, y_base, t_stamp = load_identification_data(file_name)
     sysid = SysIdentification(freq_range=freq_range,
                               f=f, N=N, p=p, m=m)
-    sysid.initialization(u=u, y=y)
+    sysid.initialization(u_sysid[:, :], y=y_arm3)
     sysid.identify_system()
+    # sysid.initialization(u=u_sysid[2:3, :], y=y_arm2)
+    # sysid.identify_system()
+    # # sysid.initialization(u=u_sysid, y=y_arm3)
+    # sysid.identify_system()
     
     print('here')
     # TODO: compare to the true value 
@@ -22,8 +25,9 @@ def main(file_name):
                                   params=sysid.params,freq_range=freq_range, 
                                   f_stamp = sysid.f_stamp, G_cov = sysid.G_cov
                                   )
+    visualization.plot_segment_signal(y_arm1[0:6, :], t_stamp)
     visualization.plot()
 
 
 if __name__ == '__main__':
-    main(file_name='test')
+    main(file_name='3Hz_mapping_5_5')
